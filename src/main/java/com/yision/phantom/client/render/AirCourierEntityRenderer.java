8458ee489341b58a43f81b9e4faa515b00f582c3
Nobodiiiii <mmu503690@gmail.com>
2026-07-05 13:54:17 +0800

    幻翼

--- src/main/java/com/yision/phantom/client/render/AirCourierEntityRenderer.java
+++ src/main/java/com/yision/phantom/client/render/AirCourierEntityRenderer.java
@@ -3,9 +3,7 @@ package com.yision.phantom.client.render;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
-import com.yision.phantom.CreatePhantom;
 import com.yision.phantom.entity.courier.AirCourierEntity;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.PhantomModel;
 import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.model.geom.ModelPart;
@@ -14,15 +12,10 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
-import net.minecraft.client.resources.model.BakedModel;
-import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.monster.Phantom;
-import net.minecraft.world.item.ItemDisplayContext;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
@@ -30,8 +23,6 @@ public class AirCourierEntityRenderer extends EntityRenderer<AirCourierEntity> {
 	private static final ResourceLocation PHANTOM_TEXTURE = new ResourceLocation("textures/entity/phantom.png");
 	private static final ResourceLocation PHANTOM_EYES_TEXTURE =
 		new ResourceLocation("textures/entity/phantom_eyes.png");
-	private static final ModelResourceLocation PACKAGE_MODEL = new ModelResourceLocation(
-		CreatePhantom.asResource("mini_phantom_attached_package"), "inventory");
 	private static final int EYES_LIGHT = 15728640;
 	private static final float CRUISE_SCALE = 0.58f;
 	private static final float ACTIVE_SCALE = 0.66f;
@@ -40,13 +31,8 @@ public class AirCourierEntityRenderer extends EntityRenderer<AirCourierEntity> {
 	private static final float MODEL_Z_OFFSET = 0.1875f;
 	private static final float PHANTOM_MODEL_Y_TRANSLATE = 1.3125f;
 	private static final float LIVING_MODEL_Y_TRANSLATE = -1.501f;
-	private static final float PACKAGE_X_OFFSET = -0.03125f;
-	private static final float PACKAGE_Y_OFFSET = -0.375f;
-	private static final float PACKAGE_Z_OFFSET = -0.265625f;
 
 	private final PhantomModel<RenderPhantom> phantomModel;
-	private final ModelPart phantomBody;
-	private final ItemRenderer itemRenderer;
 	@Nullable
 	private ClientLevel cachedLevel;
 	@Nullable
@@ -55,9 +41,6 @@ public class AirCourierEntityRenderer extends EntityRenderer<AirCourierEntity> {
 	public AirCourierEntityRenderer(EntityRendererProvider.Context context) {
 		super(context);
 		this.phantomModel = new PhantomModel<>(context.bakeLayer(ModelLayers.PHANTOM));
-		this.phantomBody = phantomModel.root()
-			.getChild("body");
-		this.itemRenderer = context.getItemRenderer();
 		this.shadowRadius = 0.45f;
 	}
 
@@ -82,7 +65,6 @@ public class AirCourierEntityRenderer extends EntityRenderer<AirCourierEntity> {
 		applyModelTransform(entity, poseStack);
 		VertexConsumer bodyBuffer = buffer.getBuffer(phantomModel.renderType(PHANTOM_TEXTURE));
 		phantomModel.renderToBuffer(poseStack, bodyBuffer, packedLight, OverlayTexture.NO_OVERLAY, 1, 1, 1, 1);
-		renderPackage(entity, poseStack, buffer, packedLight);
 		VertexConsumer eyesBuffer = buffer.getBuffer(RenderType.eyes(PHANTOM_EYES_TEXTURE));
 		phantomModel.renderToBuffer(poseStack, eyesBuffer, EYES_LIGHT, OverlayTexture.NO_OVERLAY, 1, 1, 1, 1);
 		poseStack.popPose();
@@ -116,24 +98,6 @@ public class AirCourierEntityRenderer extends EntityRenderer<AirCourierEntity> {
 			.getAllParts()
 			.forEach(ModelPart::resetPose);
 		phantomModel.setupAnim(phantom, 0.0f, 0.0f, courier.tickCount + partialTick, 0.0f, 0.0f);
-	}
-
-	private void renderPackage(AirCourierEntity entity, PoseStack poseStack, MultiBufferSource buffer, int packedLight) {
-		if (entity.getPackage()
-			.isEmpty()) {
-			return;
-		}
-
-		BakedModel packageModel = Minecraft.getInstance()
-			.getModelManager()
-			.getModel(PACKAGE_MODEL);
-
-		poseStack.pushPose();
-		phantomBody.translateAndRotate(poseStack);
-		poseStack.translate(PACKAGE_X_OFFSET, PACKAGE_Y_OFFSET, PACKAGE_Z_OFFSET);
-		itemRenderer.render(ItemStack.EMPTY, ItemDisplayContext.NONE, false, poseStack, buffer, packedLight,
-			OverlayTexture.NO_OVERLAY, packageModel);
-		poseStack.popPose();
 	}
 
 	private @Nullable RenderPhantom getOrCreateRenderPhantom(Level level) {
